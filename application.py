d959cb4d9f90ab96b3ee897cde6ceedc03fda999
Dannirux <dannirux@gmail.com>
2022-08-22 12:25:49 -0500

    aws

--- application.py
+++ application.py
@@ -2,15 +2,13 @@ from flask import Flask, json, jsonify, request
 from test import chatbot_response
 import ast
 application = Flask(__name__)
-app = application
 
-
-@app.route("/")
+@application.route("/")
 def hello_world():
     return "<p>Hello, World! this is chatbot api</p>"
 
 
-@app.route('/', methods=['GET', 'POST'])
+@application.route('/', methods=['GET', 'POST'])
 def returnChatbotResponse():
     if request.method == 'POST':
         userQuery = (request.args.get('userQuery'))
@@ -23,5 +21,7 @@ def returnChatbotResponse():
         return "<p>Please use proper API POST request call for chatbot response</p>"
 
 
+"""
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    application.run(host="0.0.0.0", port=5000, debug=True)
+ """
