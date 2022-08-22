791653534e129e101fd1cf7eecbf5f5ec5de5984
Dannirux <dannirux@gmail.com>
2022-08-22 13:08:45 -0500

    aws

--- application.py
+++ application.py
@@ -22,5 +22,5 @@ def returnChatbotResponse():
 
 
 if __name__ == '__main__':
-    from waitress import serve
-    serve(application, host="0.0.0.0", port=5000)
+    application.debug = True
+    application.run()
